f5d92f9e84a7ea7dace7363fb7dbb0d140138833
y-du <42994541+y-du@users.noreply.github.com>
2017-11-10 12:17:25 +0100

    rm unneeded code

--- libpurecoollink/utils.py
+++ libpurecoollink/utils.py
@@ -2,33 +2,6 @@
 import json
 import base64
 from Crypto.Cipher import AES
-from .const import DYSON_PURE_HOT_COOL_LINK_TOUR, DYSON_360_EYE
-
-
-def support_heating(product_type):
-    """Return True if device_model support heating mode, else False.
-
-    :param product_type Dyson device model
-    """
-    if product_type in [DYSON_PURE_HOT_COOL_LINK_TOUR]:
-        return True
-    return False
-
-
-def is_heating_device(json_payload):
-    """Return true if this json payload is a hot+cool device."""
-    if json_payload['ProductType'] in [DYSON_PURE_HOT_COOL_LINK_TOUR]:
-        return True
-    return False
-
-
-def printable_fields(fields):
-    """Return printable fields.
-
-    :param fields list of tuble with (label, vallue)
-    """
-    for field in fields:
-        yield field[0]+"="+field[1]
 
 
 def unpad(string):
@@ -49,10 +22,3 @@ def decrypt_password(encrypted_password):
     json_password = json.loads(unpad(
         cipher.decrypt(base64.b64decode(encrypted_password)).decode('utf-8')))
     return json_password["apPasswordHash"]
-
-
-def is_360_eye_device(json_payload):
-    """Return true if this json payload is a Dyson 360 Eye device."""
-    if json_payload['ProductType'] == DYSON_360_EYE:
-        return True
-    return False
