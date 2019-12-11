f1552d5077aacb07681741bccbebd54e3df8d083
y-du <42994541+y-du@users.noreply.github.com>
2019-12-11 16:13:36 +0100

    fix logger name

--- dyson/cloud_api_monitor.py
+++ dyson/cloud_api_monitor.py
@@ -23,7 +23,7 @@ import time, requests, cc_lib
 from threading import Thread
 
 
-logger = root_logger.getChild(__name__)
+logger = root_logger.getChild(__name__.split(".", 1)[-1])
 
 
 class CloudApiMonitor(Thread):
