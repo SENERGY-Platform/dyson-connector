a589c2ae782fd9e5c50e41a8659c87543ac1e158
y-du <42994541+y-du@users.noreply.github.com>
2020-01-07 16:10:07 +0100

    add, remove and update devices on platform

--- dyson/cloud_monitor.py
+++ dyson/cloud_monitor.py
@@ -17,9 +17,7 @@
 from .configuration import config
 from .logger import root_logger
 from .device_manager import DeviceManager
-# from .device import DysonDevice, dyson_map
-# from .session import SessionManager
-from libpurecoollink.utils import decrypt_password
+from .types.device import device_type_map
 import time, requests, cc_lib
 from threading import Thread
 
@@ -97,20 +95,49 @@ class CloudMonitor(Thread):
             self.__evaluate(unknown_devices)
             time.sleep(config.Cloud.poll_interval)
 
-    def __evaluate(self, unknown_devices):
-        missing_devices, new_devices, changed_devices = diff(self.__device_manager.devices, unknown_devices)
+    def __evaluate(self, queried_devices):
+        missing_devices, new_devices, changed_devices = diff(self.__device_manager.devices, queried_devices)
         if missing_devices:
+            futures = list()
             for device_id in missing_devices:
                 logger.info("can't find '{}'".format(device_id))
+                futures.append((device_id, self.__client.deleteDevice(device_id, asynchronous=True)))
+            for device_id, future in futures:
+                future.wait()
+                try:
+                    future.result()
+                    self.__device_manager.delete(device_id)
+                except cc_lib.client.DeviceDeleteError:
+                    pass
         if new_devices:
+            futures = list()
             for device_id in new_devices:
-                logger.info("found '{}' with id '{}'".format(unknown_devices[device_id][0]["name"], device_id))
+                logger.info("found '{}' with id '{}'".format(queried_devices[device_id][0]["name"], device_id))
+                device = device_type_map[queried_devices[device_id][1]["type"]](device_id, queried_devices[device_id][1]["pw"], **queried_devices[device_id][0])
+                futures.append((device, self.__client.addDevice(device, asynchronous=True)))
+            for device, future in futures:
+                future.wait()
+                try:
+                    future.result()
+                    self.__device_manager.add(device)
+                except (cc_lib.client.DeviceAddError, cc_lib.client.DeviceUpdateError):
+                    pass
         if changed_devices:
+            futures = list()
             for device_id in changed_devices:
-                logger.info("name of '{}' changed to '{}'".format(device_id, unknown_devices[device_id][0]["name"]))
+                logger.info("name of '{}' changed to '{}'".format(device_id, queried_devices[device_id][0]["name"]))
+                device = self.__device_manager.get(device_id)
+                prev_device_name = device.name
+                device.name = queried_devices[device_id][0]["name"]
+                futures.append((device, prev_device_name, self.__client.updateDevice(device, asynchronous=True)))
+            for device, prev_device_name, future in futures:
+                future.wait()
+                try:
+                    future.result()
+                except cc_lib.client.DeviceUpdateError:
+                    device.name = prev_device_name
         if any((missing_devices, new_devices, changed_devices)):
-            pass
-            # try:
-            #     self.__client.syncHub(list(self.__device_manager.devices.values()), asynchronous=True)
-            # except cc_lib.client.HubError:
-            #     pass
+            try:
+                self.__client.syncHub(list(self.__device_manager.devices.values()), asynchronous=True)
+            except cc_lib.client.HubError:
+                pass
