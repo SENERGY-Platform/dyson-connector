b632ccf7a70da500415a196b403a67fa4374624e
y-du <42994541+y-du@users.noreply.github.com>
2020-01-07 13:17:01 +0100

    reimplementation

--- dyson/cloud_monitor.py
+++ dyson/cloud_monitor.py
@@ -15,10 +15,11 @@
 """
 
 from .configuration import config
-from dyson.device import DysonDevice, dyson_map
-from dyson.session import SessionManager
+from .logger import root_logger
+from .device_manager import DeviceManager
+# from .device import DysonDevice, dyson_map
+# from .session import SessionManager
 from libpurecoollink.utils import decrypt_password
-from dyson.logger import root_logger
 import time, requests, cc_lib
 from threading import Thread
 
@@ -26,94 +27,87 @@ from threading import Thread
 logger = root_logger.getChild(__name__.split(".", 1)[-1])
 
 
+def getApiCredentials():
+    body = {
+        "Email": config.Account.email,
+        "Password": config.Account.pw
+    }
+    http_resp = requests.post(
+        url="https://{}/{}{}".format(config.Cloud.host, config.Cloud.auth_endpt, config.Account.country),
+        json=body,
+        verify=False
+    )
+    if http_resp.status_code == 200:
+        credentials = http_resp.json()
+        config.Cloud.user = credentials.get('Account')
+        config.Cloud.pw = credentials.get('Password')
+        return True
+    logger.error(
+        "could not retrieve dyson cloud credentials - '{}' - '{}'".format(http_resp.status_code, http_resp.raw))
+    return False
+
+
+def apiQueryDevices():
+    unknown_devices = dict()
+    http_resp = requests.get(
+        url="https://{}/{}".format(config.Cloud.host, config.Cloud.provisioning_endpt),
+        auth=(config.Cloud.user, config.Cloud.pw),
+        verify=False
+    )
+    if http_resp.status_code == 200:
+        devices = http_resp.json()
+        for device in devices:
+            try:
+                unknown_devices[device['Serial']] = {
+                    "name": device["Name"],
+                    "type": device["ProductType"],
+                    "credentials": device["LocalCredentials"]
+                }
+            except KeyError:
+                logger.error("missing device serial or malformed message - '{}'".format(device))
+    return unknown_devices
+
+
+def diff(known, unknown):
+    known_set = set(known)
+    unknown_set = set(unknown)
+    missing = known_set - unknown_set
+    new = unknown_set - known_set
+    changed = {key for key in known_set & unknown_set if dict(known[key]) != unknown[key]}
+    return missing, new, changed
+
+
 class CloudMonitor(Thread):
-    def __init__(self, client: cc_lib.client.Client):
+    def __init__(self, device_manager: DeviceManager, client: cc_lib.client.Client):
         super().__init__(name=__class__.__name__, daemon=True)
+        self.__device_manager = device_manager
         self.__client = client
-        self.__know_devices = list()
 
     def run(self):
         if not (config.Cloud.user and config.Cloud.pw):
-            while not self._getApiCredentials():
+            while not getApiCredentials():
                 logger.info("retry in 30s")
                 time.sleep(30)
         while True:
-            unknown_devices = self._apiQueryDevices()
+            unknown_devices = apiQueryDevices()
             self._evaluate(unknown_devices)
             time.sleep(config.Cloud.poll_interval)
 
-    def _getApiCredentials(self):
-        body = {
-            "Email": config.Account.email,
-            "Password": config.Account.pw
-        }
-        http_resp = requests.post(
-            url="https://{}/{}{}".format(config.Cloud.host, config.Cloud.auth_endpt, config.Account.country),
-            json=body,
-            verify=False
-        )
-        if http_resp.status_code == 200:
-            credentials = http_resp.json()
-            config.Cloud.user = credentials.get('Account')
-            config.Cloud.pw = credentials.get('Password')
-            return True
-        logger.error("could not retrieve dyson cloud credentials - '{}' - '{}'".format(http_resp.status_code, http_resp.raw))
-        return False
-
-    def _apiQueryDevices(self):
-        unknown_devices = dict()
-        http_resp = requests.get(
-            url="https://{}/{}".format(config.Cloud.host, config.Cloud.provisioning_endpt),
-            auth=(config.Cloud.user, config.Cloud.pw),
-            verify=False
-        )
-        if http_resp.status_code == 200:
-            devices = http_resp.json()
-            for device in devices:
-                try:
-                    unknown_devices[device['Serial']] = device
-                except KeyError:
-                    logger.error("missing device serial or malformed message - '{}'".format(device))
-        return unknown_devices
-
-    def _diff(self, known, unknown):
-        known_set = set(known)
-        unknown_set = set(unknown)
-        missing = known_set - unknown_set
-        new = unknown_set - known_set
-        return missing, new
-
     def _evaluate(self, unknown_devices):
-        missing_devices, new_devices = self._diff(self.__know_devices, unknown_devices)
+        missing_devices, new_devices, changed_devices = diff(self.__device_manager.devices, unknown_devices)
         if missing_devices:
-            for missing_device_id in missing_devices:
-                logger.info("can't find '{}'".format(missing_device_id))
-                try:
-                    Client.disconnect(missing_device_id)
-                except AttributeError:
-                    DevicePool.remove(missing_device_id)
-                SessionManager.delRemoteDevice(missing_device_id)
+            for device_id in missing_devices:
+                logger.info("can't find '{}'".format(device_id))
         if new_devices:
-            for new_device_id in new_devices:
-                try:
-                    dyson_data = dyson_map[unknown_devices[new_device_id]['ProductType']]
-                    dyson_device = DysonDevice(
-                        new_device_id,
-                        dyson_data['type'],
-                        dyson_data['name'],
-                        decrypt_password(unknown_devices[new_device_id]['LocalCredentials']),
-                        unknown_devices[new_device_id]['ProductType'],
-                        unknown_devices[new_device_id]['ScaleUnit']
-                    )
-                    dyson_device.addTag('manufacturer', 'Dyson')
-                    count = ''
-                    for tag in dyson_data['tags']:
-                        dyson_device.addTag('type{}'.format(count), tag)
-                        if not count:
-                            count = 0
-                        count = count + 1
-                    logger.info("found '{}' with id '{}'".format(dyson_device.name, dyson_device.id))
-                    SessionManager.addRemoteDevice(dyson_device)
-                except KeyError:
-                    logger.error("missing device data or malformed message - '{}'".format(unknown_devices[new_device_id]))
-        self.__know_devices = unknown_devices.keys()
+            for device_id in new_devices:
+                logger.info("found '{}' with id '{}'".format(unknown_devices[device_id]["name"], device_id))
+                logger.debug(decrypt_password(unknown_devices[device_id]['credentials']))
+        if changed_devices:
+            for device_id in changed_devices:
+                logger.info("name of '{}' changed to '{}'".format(device_id, unknown_devices[device_id]["name"]))
+        if any((missing_devices, new_devices, changed_devices)):
+            pass
+            # try:
+            #     self.__client.syncHub(list(self.__device_manager.devices.values()), asynchronous=True)
+            # except cc_lib.client.HubError:
+            #     pass
